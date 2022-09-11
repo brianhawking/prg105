c5e35eeaa9a91266b4470db9ffb8499e77eddb57
Brian Veitch <beveitch@gmail.com>
2022-09-10 22:40:00 -0500

    completed chapter 1 printing

---
+++ Ch1_Assignment1_Printing/lyrics.py
@@ -0,0 +1,20 @@
+print("Everyone Wants To Rule the World by Tears for Fears\n")
+
+print("Welcome to your life")
+print("There's no turning back")
+print("Even while we sleep")
+print("We will find you\n")
+
+print("Acting on your best behaviour")
+print("Turn your back on Mother Nature")
+print("Everybody wants to rule the world\n")
+
+print("It's my own design")
+print("It's my own remorse")
+print("Help me to decide")
+print("Help me make the\n")
+
+
+print("Most of freedom and of pleasure")
+print("Nothing ever lasts forever")
+print("Everybody wants to rule the world")
