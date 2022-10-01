f83797ed474485fa53702f29be5a61ddb105b063
Brian Veitch <beveitch@gmail.com>
2022-09-30 21:33:53 -0500

    completed reading file assignment

--- Chapter6_Reading_File/ReadingFile/reading_file.py
+++ Chapter6_Reading_File/ReadingFile/reading_file.py
@@ -10,7 +10,34 @@ At the end, display the total, count and average of the values.
 
 
 def main():
-
     # Open file
-    file = open('numeric_data.txt', 'w')
-    file.close()
+    file = open('numeric_data.txt', 'r')
+
+    counter = 0
+    total = 0
+
+    line = file.readline().rstrip('\n')
+
+    while line != '':
+        counter += 1
+        line = float(line.replace(',', ''))
+        total += line
+
+        # Get next line
+        line = file.readline().rstrip('\n')
+
+    print("{:<18} {:>12}".format(
+        "Total:",
+        f"{total:,.2f}"
+    ))
+    print("{:<18} {:>12}".format(
+        "Number of Entries:",
+        f"{counter}"
+    ))
+    print("{:<18} {:>12}".format(
+        "Average:",
+        f"{(total / counter):,.2f}"
+    ))
+
+# ======== Start program ===============
+main()
