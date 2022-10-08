0f6f2afb11019e8c0cf7d674760cdbf50a1bc42d
Brian Veitch <beveitch@gmail.com>
2022-10-08 13:09:23 -0500

    update formatting

--- Chapter8_Rainfall/rainfall/rainfall.py
+++ Chapter8_Rainfall/rainfall/rainfall.py
@@ -6,12 +6,22 @@ display formatted results on the screen.
 
 
 def main():
+    # Initialize
     total = 0
     count = 0
+
+    # Check to make sure the file can be opened
     try:
         file = open('rainfall-totals.txt', 'r')
+
+        # Loop through all lines in file
         for line in file:
+            # Strip the '\n' from the line before splitting
             components = line.rstrip('\n').split()
+
+            # Try to convert rainfall to float
+            # I prefer this way over "remove decimals, commas, etc." and see if
+            # resulting value is a digit. I assume that happens in float() anyway
             try:
                 total += float(components[1])
                 count += 1
@@ -19,13 +29,14 @@ def main():
             except Exception:
                 print(f"{components[0]} - {components[1]} (not a valid number)")
 
+        file.close()
+
+        # Format data for user
         if count != 0:
-            print(f"\nTotal - {total:.2f}in")
-            print(f"Average - {(total / count):.2f}in / month")
+            print(f"\nThe Total rainfall for {count} months is - {total:.2f}in")
+            print(f"The Average rainfall is - {(total / count):.2f}in / month")
         else:
             print("No valid rainfalls were found.")
-
-        file.close()
 
     except Exception:
         print("Error trying to open file.")
